e87a6e3cabea276e9a0dd3ddb3c7b7d742e41078
Иван Писарев <ivan_chel@mail.ru>
2022-04-12 22:12:18 +0500

    Default permission classes IsAuthenticatedOrReadOnly

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -101,3 +101,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ],
+}
