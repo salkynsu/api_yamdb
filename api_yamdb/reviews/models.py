4dd290fb48ed1c9038a9a1d84355a60f466dd8a8
Evgeniy Semin <evgeniy.semin91@gmail.com>
2022-04-14 22:00:16 +0300

    fixed pub_date field

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -53,7 +53,7 @@ class Review(models.Model):
         User, on_delete=models.CASCADE, related_name="review"
     )
     score = models.PositiveSmallIntegerField()
-    pub_date = models.DateField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
@@ -64,4 +64,4 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="comment"
     )
-    pub_date = models.DateField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
