f966296c5dd9cae604a1fb980af2e97391d7818d
salkynsu <salkyn.su@gmail.com>
2022-04-17 19:07:28 +0300

    Merge conflict resolved

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,9 +4,17 @@ from rest_framework import permissions
 class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
-            request.method in permissions.SAFE_METHODS or
-            request.user and
-            request.user.role == "admin"
+            request.method in permissions.SAFE_METHODS
+            or request.user
+            and request.user.role == "admin"
+        )
+
+
+class AdminOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == "admin"
         )
         # return (
         #     request.user.is_authenticated and
